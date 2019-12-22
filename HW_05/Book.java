a14fcaf41d2b47bd9ca616edbc6a726c31827399
Him HEY <58143774+him123hey@users.noreply.github.com>
2019-12-22 10:28:02 +0700

    homeworkjava

---
+++ HW_05/Book.java
@@ -0,0 +1,5 @@
+package HW_05;
+
+public class Book {
+
+}
---
+++ HW_05/Demo.java
@@ -0,0 +1,10 @@
+package HW_05;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
